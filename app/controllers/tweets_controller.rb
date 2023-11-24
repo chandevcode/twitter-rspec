bcbe7fad02008ff9d04feac9589d666110bea017
chan <baguschandradp@gmail.com>
2023-11-24 22:53:12 +0800

    feat: add tweet

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,20 +1,30 @@
 class TweetsController < ApplicationController
-  before_action :set_params, except: %i[index create]
-  def index; end
+  before_action :set_tweet, only: %i[show edit update destroy]
 
-  def new
+  def index
+    @tweets = Tweet.all.order(created_at: :desc)
     @tweet = Tweet.new
   end
 
-  def create; end
+  def new; end
+
+  def create
+    @tweet = current_user.tweets.build(tweet_params)
+
+    if @tweet.save
+      redirect_to root_path, notice: 'Tweet was created'
+    else
+      render :new, status: :unprocessable_entity, notice: 'Something went wrong'
+    end
+  end
 
   private
 
-  def set_params
+  def set_tweet
     @tweet = Tweet.find(params[:id])
   end
 
-  def set_tweet_params
+  def tweet_params
     params.require(:tweets).permit(:body)
   end
 end
